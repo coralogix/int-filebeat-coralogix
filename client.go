4c63456077180be705aaa2007efc559d7efe143c
ofirshmuel <ofir@coralogix.com>
2021-10-05 11:13:49 +0300

    add logenries test array of json, client.go

--- client.go
+++ client.go
@@ -394,7 +394,7 @@ func makeEvent(v *beat.Event) map[string]json.RawMessage {
 
 	}
 
-	cxParamsInterface := []map[string]interface{}{{"cxParams": "b", "abc": "ab"}}
+	cxParamsInterface := []map[string]interface{}{{"timestamp": epochTimeStr, "severity": "4", "text": "test text"}}
 
 	b, err = json.Marshal(cxParamsInterface)
 	if err != nil {
